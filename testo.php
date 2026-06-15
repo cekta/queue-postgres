472415fce4092184c79be33dc0df65e1c65bde72
Evgeniy Kuvshinov <smpl@itmywork.com>
2026-06-16 00:04:11 +0500

    fail on warnings

--- testo.php
+++ testo.php
@@ -8,6 +8,15 @@ use Testo\Codecov\CodecovPlugin;
 use Testo\Codecov\Report\PhpUnitXmlReport;
 use Testo\Convention\NamingConventionPlugin;
 
+set_error_handler(function ($errno, $errstr, $errfile, $errline) {
+    // Преобразуем предупреждения (warnings), уведомления (notices) и ошибки пользователя (user-errors) в исключения
+    if (in_array($errno, [E_WARNING, E_NOTICE, E_USER_WARNING, E_USER_NOTICE, E_DEPRECATED, E_USER_DEPRECATED], true)) {
+        throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+    }
+    // Для фатальных ошибок продолжаем использовать стандартный обработчик
+    return false;
+});
+
 return new ApplicationConfig(
     src: ["src"],
     suites: [
